7239d5e6a9e7ac35baf7c8cb942275839d199330
bharatcs0550 <bharatcs0550@gmail.com>
2026-07-17 14:16:00 +0530

    test: add fixture-based tests for sum and max validation

--- test_sample.py
+++ test_sample.py
@@ -1,3 +1,17 @@
+import pytest
+
+@pytest.fixture
+def sample_data():
+    return [10 ,20, 30]
+
+def test_sum(sample_data):
+    assert sum(sample_data) == 60
+
+def test_max(sample_data):
+    assert max(sample_data) == 30   
+
+
+
 def add(a, b):
     return a + b
 
